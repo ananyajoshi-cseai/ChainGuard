e783df034189c049379b4ae78a27aa5baadf153f
Ananya Joshi <ananyajoshi.cseai@gmail.com>
2026-09-02 18:04:59 +0530

    Allow deployed frontend origin

--- backend/main.py
+++ backend/main.py
@@ -28,9 +28,10 @@ app = FastAPI(title="ChainGuard API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://127.0.0.1:5500",
-        "http://localhost:5500"
-    ],
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
+    "https://chain-guard-ruddy.vercel.app"
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
